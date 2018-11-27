a6cb0321a0196fab87345fbe3407aa29cdf0948e
hugo hadfield <hadfield.hugo@gmail.com>
2018-11-27 16:14:40 +0000

    Updated to use static rendering

--- pyganja/GanjaScene.py
+++ pyganja/GanjaScene.py
@@ -7,7 +7,7 @@ class GanjaScene:
     def __init__(self):
         self.internal_list = []
 
-    def add_object(self, mv_array, color=int('AA000000', 16), label=None):
+    def add_object(self, mv_array, color=int('AA000000', 16), label=None, static=True):
         if label is not None:
             try:
                 assert isinstance(label, str)
@@ -17,7 +17,7 @@ class GanjaScene:
         self.internal_list.append(color)
         self.internal_list.append([i for i in mv_array])
 
-    def add_objects(self, mv_list, color=int('AA000000', 16), label=None):
+    def add_objects(self, mv_list, color=int('AA000000', 16), label=None, static=True):
         if label is not None:
             try:
                 assert isinstance(label, str)
@@ -25,11 +25,19 @@ class GanjaScene:
             except:
                 raise ValueError('Labels must be strings')
         self.internal_list.append(color)
+        static_list = []
         for mv_array in mv_list:
-            self.internal_list.append([i for i in mv_array])
+            if static:
+                static_list.append([i for i in mv_array])
+            else:
+                self.internal_list.append([i for i in mv_array])
+        if static:
+            self.internal_list.append({'data': static_list})
 
     def __repr__(self):
-        return json.dumps(self.internal_list)
+        new_str = str.replace(json.dumps(self.internal_list), '''"data": ''', 'data:')
+        new_str = str.replace(new_str, ']}', """].map(x=>x.length==32?new Element(x):x)}""")
+        return new_str
 
     def save_to_file(self, filename):
         with open(filename, 'w') as fobj:
--- test_pyganja.py
+++ test_pyganja.py
@@ -5,7 +5,13 @@ from pyganja import *
 class TestCEFDrawing(unittest.TestCase):
     def test_draw_lines(self):
         from clifford.tools.g3c import random_line
-        draw_objects([random_line().value for i in range(10)])
+        draw_objects([random_line() for i in range(10)])
+
+    def test_print_scene(self):
+        from clifford.tools.g3c import random_line
+        gs = GanjaScene()
+        gs.add_objects([random_line() for i in range(2)])
+        print(gs)
 
 
 if __name__ == '__main__':
